862378f841d59606f59d4613410a614929c789d4
이은석 <hspup3@gmail.com>
2025-08-11 11:51:15 +0900

    chore: 초기 안정화 패치 - App 렌더링 복구 및 임시 화면 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './index.css';  // 추가된 부분
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
